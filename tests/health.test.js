2f8c81ca91a74d65dbac8f312eabb5d2a2ead3e9
Michael <michael.picard14@gmail.com>
2025-08-13 21:20:04 +0200

    fix added the app in the request for testing

--- tests/health.test.js
+++ tests/health.test.js
@@ -3,7 +3,7 @@ import app from "../app/server.js";
 
 describe('GET /health', () => {
     it('GET /health', async() => {
-        const response = await request.get("/health");
+        const response = await request(app).get("/health");
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('status', 'ok');
     });
