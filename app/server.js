827b1ea74cb3650d92e18e5b93b630b51e7b89ed
Michael <michael.picard14@gmail.com>
2025-08-14 18:58:24 +0200

    fix: the status

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/health", (req,res) => {
-    res.status(200).json("ok");
+    res.status(200).json({ status: 'ok' });
 });
 
 if (process.env.NODE_END !== "test"){
