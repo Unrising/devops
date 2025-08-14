b159350eca6df376b61dcb97cdb066e233edcd58
Michael <michael.picard14@gmail.com>
2025-08-14 19:01:13 +0200

    fix: NODE_END

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ app.get("/health", (req,res) => {
     res.status(200).json({ status: 'ok' });
 });
 
-if (process.env.NODE_END !== "test"){
+if (process.env.NODE_ENV !== "test"){
     app.listen(PORT, () => {
         console.log(`The server is running on Port ${PORT}`);
     });
